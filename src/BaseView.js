a91c71118f030806485dcd57dbc71017804fcf2e
Łukasz Rodziewicz <lukasz@rodziewicz.com.pl>
2013-06-22 00:59:27 +0200

    Adding basic functionality for adding and getting view at the end of the collection or at given offset.

--- src/BaseView.js
+++ src/BaseView.js
@@ -3,13 +3,108 @@ define(['lodash', 'backbone'], function(_, Backbone) {
     'use strict';
 
     var BaseView = function(options) {
-        this._childViews = {};
+        this._views = [];
 
         Backbone.View.apply(this, [options]);
     };
 
     _.extend(BaseView.prototype, Backbone.View.prototype, {
 
+        _prepareEntry: function(parameters) {
+            var entry;
+
+            // If given an instance of Backbone.View
+            if(parameters instanceof Backbone.View) {
+                entry = {
+                    name: parameters.cid,
+                    view: parameters
+                };
+            }
+            // If given an array of view objects
+            else if (_.isArray(parameters)) {
+                entry = [];
+                for (var i = 0; i < parameters.length; i++) {
+                    this._checkIfValidView(parameters[i]);
+
+                    entry.push(this._prepareEntry(parameters[i]));
+                }
+            }
+            // If given a literal object where each view have have name like { "name": view }
+            else if (_.isPlainObject(parameters)) {
+                entry = [];
+                for (var name in parameters) {
+                    this._checkIfValidView(parameters[name]);
+
+                    entry.push({
+                        name: name,
+                        view: parameters[name]
+                    });
+                }
+            }
+            else {
+                throw "Parameter should have to either Backbone.View instance, array of Backbone.Views or object list of Backbone.Views";
+            }
+
+            return entry;
+        },
+
+        _checkIfValidView: function (view) {
+            if(!view instanceof Backbone.View) throw "View have to be an instance of Backbone.View";
+        },
+
+        add: function(parameters, at) {
+            var entry = this._prepareEntry(parameters);
+
+            if(at) {
+                // If array is not large enough expand it
+                if(at > this._views.length - 1) {
+                    if(_.isArray(entry)) {
+                        this._views.push.apply(this._views, entry);
+                    } else {
+                        this._views[at] = entry;
+                    }
+                }
+                // Move items around if point of insertion has to be into an existing space
+                else {
+                    if(_.isArray(entry)) {
+                        //this._views.splice.apply(this._views, (function () { entry.splice(0, 0, at, 0); return entry;})());
+                        this._views.splice.apply(this._views, [at, 0].concat(entry));
+                    }
+                    else {
+                        this._views.splice(at, 0, entry);
+                    }
+                }
+
+                return this._views.length;
+            }
+
+            // If place is not set
+            if(_.isArray(entry)) {
+                return this._views.push.apply(this._views, entry);
+            }
+            return this._views.push(entry);
+        },
+
+        get: function(arg) {
+            var entry;
+            // Get by position
+            if (_.isNumber(arg)) {
+                entry = this._views[arg];
+            }
+            // Get by name in other case
+            else if (_.isString(arg)) {
+                entry = _.find(this._views, function(element) {
+                    return element.name === arg;
+                });
+            }
+            else throw "Expecting either number or a string";
+
+            return entry ? entry.view : void 0;
+        },
+
+        size: function () {
+            return this._views.length;
+        }
     });
 
     BaseView.extend = Backbone.View.extend;
--- test/BaseView.spec.js
+++ test/BaseView.spec.js
@@ -1,7 +1,76 @@
-define(['BaseView'], function (BaseView) {
+define(['backbone', 'BaseView'], function (Backbone, BaseView) {
+
     describe('BaseView', function () {
-        it('can be extended', function() {
-            expect(BaseView.extend({})).toBeDefined();
+
+        describe('(managing views)', function() {
+
+            var view, childView, childView2, childView3, childView4;
+
+            beforeEach(function(){
+                view = new BaseView();
+
+                childView1 = new (Backbone.View.extend({ name: 'child1'}))();
+                childView2 = new (Backbone.View.extend({ name: 'child2'}))();
+                childView3 = new (Backbone.View.extend({ name: 'child3'}))();
+                childView4 = new (Backbone.View.extend({ name: 'child4'}))();
+            });
+
+            it('can have a view added anonymously', function() {
+                expect(view.add(childView1)).toBe(1);
+
+                function Abc() {}
+                var a = new Abc();
+                expect(function(){view.add(Abc);}).toThrow();
+            });
+
+            it('can add view at given position', function() {
+                // TODO: moar tests
+                expect(view.add(childView1, 2)).toBe(3);
+            });
+
+            it('can get view from given position', function() {
+                view.add(childView1, 2);
+                expect(typeof view.get(2)).toBe('object');
+                expect(view.get(5)).toBeUndefined();
+            });
+
+            it('can have multiple views added anonymously with no possition given', function () {
+                view.add([childView1, childView2]);
+                expect(view.size()).toBe(2);
+
+                view.add([childView3, childView4]);
+                expect(view.size()).toBe(4);
+                expect(view.get(3).name).toBe('child4');
+                expect(view.get(1).name).toBe('child2');
+            });
+
+            it('can have multiple anonymous views added at given possition', function () {
+                view.add([childView2, childView1]);
+                view.add([childView4, childView3], 1);
+
+                expect(view.size()).toBe(4);
+                expect(view.get(2).name).toBe('child3');
+                expect(view.get(3).name).toBe('child1');
+            });
+
+
+            it('can add view by name', function() {
+                view.add({ "name": childView1 });
+                expect(view.get("name").name).toBe('child1');
+            });
+
+            it('can have multiple named views added at given possition', function () {
+                view.add([childView4, childView3, childView3]);
+                view.add({"c2": childView2, "c1": childView1}, 2);
+
+                expect(view.size()).toBe(5);
+                expect(view.get(3).name).toBe('child1');
+                expect(view.get(1).name).toBe('child3');
+                expect(view.get(4).name).toBe('child3');
+                expect(view.get("c2").name).toBe('child2');
+            });
+
         });
+
     });
 });
